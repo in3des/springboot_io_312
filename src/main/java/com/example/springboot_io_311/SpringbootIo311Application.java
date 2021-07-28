bfb7bf2d15245ef4d5291ad0c4a4606f242f91b7
in3des <62700347+in3des@users.noreply.github.com>
2021-07-28 20:10:56 +0400

    Initial commit

---
+++ src/main/java/com/example/springboot_io_311/SpringbootIo311Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_io_311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIo311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIo311Application.class, args);
+	}
+
+}
